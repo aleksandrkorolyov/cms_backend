35c6e34b01305380dc47ecb0a2f59732099a5f5f
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2022-12-13 10:53:01 +0100

    Added read and create

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 require("./config/database").connect();
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 
@@ -10,8 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-// Logic goes here
 
 module.exports = app;
 
@@ -25,18 +23,32 @@ app.post("/users", async (req, res) => {
     res.header('Access-Control-Allow-Headers', "*");
     res.status(200).json(user);
   } catch {
-
+    console.error(res)
   }
 })
 
+app.post("/user/add", async (req, res) => {
+  try {
+    const { first_name, last_name, role, email, password } = req.body;
 
+    encryptedPassword = await bcrypt.hash(password, 10);
 
-app.get("/welcome", (req, res) => {
-    res.status(200).send("Welcome 🙌 ");
-  });
+    const user = await User.create({
+      first_name,
+      last_name,
+      role,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    })
+    res.header('Access-Control-Allow-Headers', "*");
+    res.status(201).json(user);
+  } catch(err) {
+    console.log(err)
+  }
+})
 
     // This should be the last route else any after it won't work
-app.use("*", (req, res) => {
+    app.use("*", (req, res) => {
     res.status(404).json({
       success: "false",
       message: "Page not found",
